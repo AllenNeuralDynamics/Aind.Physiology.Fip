f8478c74cfbced06148fd2f2bbaf7b6596b5cf47
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-10-22 11:36:08 -0700

    Add tests

--- tests/test_aind_data_schema_mapper.py
+++ tests/test_aind_data_schema_mapper.py
@@ -1,72 +1,44 @@
 import sys
 import unittest
 from datetime import datetime
-from pathlib import Path
+from typing import Any
 
-from aind_data_schema.core import acquisition, instrument
-from aind_data_schema.utils import compatibility_check
-from git import Repo
+import pandas as pd
+from contraqctor.contract import DataStream
 
-from aind_physiology_fip.data_mappers import AcquisitionMapper, InstrumentMapper
+from aind_physiology_fip.data_mappers import ProtoAcquisitionMapper
 
 sys.path.append(".")
-from examples.example import mock_rig, mock_session
+
+
+class MockCsv(DataStream[pd.DataFrame, Any]):
+    _inner_data: pd.DataFrame
+
+    def _reader(self, params: Any) -> pd.DataFrame:
+        return self._inner_data
 
 
 class TestAcquisitionMapper(unittest.TestCase):
-    def setUp(self):
-        self.session = mock_session
-        self.rig = mock_rig
-        self.repository = Repo(Path("./"))
-        self.session_end_time = datetime.now()
-        self.session_directory = None
-
-        self.mapper = AcquisitionMapper()
-
-    def test_map(self):
-        mapped = self.mapper.map()
-        self.assertIsNotNone(mapped)
-
-    def test_round_trip(self):
-        mapped = self.mapper.map()
-        assert mapped is not None
-        acquisition.Acquisition.model_validate_json(mapped.model_dump_json())
-
-
-class TestInstrumentMapper(unittest.TestCase):
-    def setUp(self):
-        self.rig = mock_rig()
-        self.mapper = InstrumentMapper(
-            rig=self.rig,
+    def test_time_extraction_default(self):
+        _data_stream = MockCsv(
+            "camera_green_iso_metadata",
+            reader_params=object(),
+            description="Mock CSV data stream for testing.",
+        )
+        _data_stream._inner_data = pd.DataFrame(
+            {
+                "CpuTime": [
+                    "2025-07-18T19:03:19.000Z",
+                    "2025-07-18T19:03:20.000Z",
+                    "2025-07-18T19:03:21.000Z",
+                ],
+                "SomeOtherData": [1, 2, 3],
+            }
         )
 
-    def test_map(self):
-        mapped = self.mapper.map()
-        self.assertIsNotNone(mapped)
-
-    def test_round_trip(self):
-        mapped = self.mapper.map()
-        assert mapped is not None
-        instrument.Instrument.model_validate_json(mapped.model_dump_json())
-
-
-class TestInstrumentAcquisitionCompatibility(unittest.TestCase):
-    def setUp(self):
-        self.rig = mock_rig()
-        self.session = mock_session()
-        self.rig_mapper = InstrumentMapper(
-            rig=self.rig,
-        )
-        self.session_mapper = AcquisitionMapper()
-
-    def test_compatibility(self):
-        session_mapped = self.session_mapper.map()
-        assert session_mapped is not None
-        rig_mapped = self.rig_mapper.map()
-        assert rig_mapped is not None
-        compatibility_check.InstrumentAcquisitionCompatibility(
-            instrument=rig_mapped, acquisition=session_mapped
-        ).run_compatibility_check()
+        start_utc, end_utc = ProtoAcquisitionMapper._extract_from_df(_data_stream.read())
+        self.assertEqual(start_utc, datetime.fromisoformat("2025-07-18T19:03:19Z"))
+        self.assertEqual(end_utc, datetime.fromisoformat("2025-07-18T19:03:21+00:00"))
 
 
 if __name__ == "__main__":
